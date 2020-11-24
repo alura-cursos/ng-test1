ec93d0cafcc7dbd958d41a2312c4042c1d5e190b
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-11-24 13:21:13 +0000

    video4.1

---
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -0,0 +1,14 @@
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+import { LikeWidgetComponent } from './like-widget.component';
+
+describe(LikeWidgetComponent.name, () => {
+  let component: LikeWidgetComponent = null;
+
+  beforeEach(() => {
+
+  });
+
+  it('', () => {
+
+  });
+});
