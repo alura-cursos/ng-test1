537f4351b96051cbfecc2fcbe02ab54a7646be2f
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-11-26 17:39:44 +0000

    video4.10

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -32,12 +32,10 @@ describe(LikeWidgetComponent.name, () => {
   });
 
   it(`#${LikeWidgetComponent.prototype.like.name}
-    should trigger emission when called`, done => {
+    should trigger emission when called`, () => {
+      spyOn(component.liked, 'emit');
       fixture.detectChanges();
-      component.liked.subscribe(() => {
-        expect(true).toBeTrue();
-        done();
-      });
       component.like();
+      expect(component.liked.emit).toHaveBeenCalled();
   });
 });
